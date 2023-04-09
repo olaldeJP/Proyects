9a3a95d9450b9dde00fd0f3fa700c95e921e3339
olaldeJP <olaldejp@gmail.com>
2023-04-09 18:11:03 +0200

    09/04 cambios

---
+++ Proyecto/scripVideo.js
@@ -0,0 +1,10 @@
+
+let video=document.querySelector(".elVideo")
+
+document.querySelector(".botonPlay").addEventListener("click",function(){
+    video.play();
+})
+
+document.querySelector(".botonPause").addEventListener("click",function(){
+    video.pause();
+})
