5a3469f74e49fccd304ce5697f482a73352f94e2
olaldeJP <olaldejp@gmail.com>
2023-05-12 20:14:59 +0200

    12/05/2023

--- BioHeat/scripsIndex.js
+++ BioHeat/scripsIndex.js
@@ -1,24 +1,38 @@
+const botonH=document.querySelector(".botonHamburguesa");
 const li=document.querySelectorAll(".liC");
-const ulBox=document.querySelector(".ulBox")
-const botonH=document.querySelector(".botonHamburguesa")
+const ulBox=document.querySelector(".ulBox");
+const circuloCompra=document.querySelector(".circulo");
+const cardCompras=document.querySelectorAll(".card")
+const botonCompra=document.querySelectorAll("#agregarProducto");
 let mostrar=false;
 
 
-
-botonH.addEventListener("click",function(){
+function cambiarMenuHamburguesa(){
        
-        if (mostrar == false){
-            ulBox.style="display:flex"
-            botonH.style="background-color: red;border:solid 2px black;"
-            botonH.textContent="X"
-            mostrar=true
-         }else{
-            ulBox.style="display:none "
-            botonH.textContent="V"
-            botonH.style="border: solid 2px  orange;background-color: rgb(51, 92, 51)"
-         
-            mostrar=false;
-        }
+    if (mostrar == false){
+        ulBox.style="animation: desplegarMenuHamburguesa 1s forwards;"
+        botonH.style="background-color: red;border:solid 2px black;"
+        botonH.textContent="X"
+        mostrar=true
+     }else{
+        ulBox.style="animation: guardarMenuHamburguesa 1s forwards; "
+        console.log(ulBox.style.display);
+        console.log(ulBox.style.opacity);
+        console.log("3");               
+        botonH.textContent="V"
+        botonH.style="border: solid 2px  orange;background-color: rgb(51, 92, 51)";
+        mostrar=false;
+
+}
+}
 
 
-})
+function aumentarcarro(event){
+    console.log(event.style)
+    circuloCompra.textContent++
+    
+
+}
+
+
+botonH.addEventListener("click",cambiarMenuHamburguesa);
