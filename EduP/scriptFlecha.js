5b5faa6328d6be6a9371e0f28c23c3d3f5ca602c
olaldeJP <olaldejp@gmail.com>
2023-05-18 18:24:49 +0200

    18/05/2023

---
+++ EduP/scriptFlecha.js
@@ -0,0 +1,8 @@
+window.onscroll = () => {
+  if (document.documentElement.scrollTop > 100) {
+    document.querySelector(".contenedor").classList.add("mostrar");
+  } else {
+    document.querySelector(".contenedor").classList.remove("mostrar");
+  }
+  console.log("hola");
+};
