9c27a617901d9895028863c33325934b98dd75df
olaldeJP <olaldejp@gmail.com>
2023-11-01 16:53:08 -0300

    config

--- BackEnd/clase3/productManager/product.js
+++ BackEnd/clase3/productManager/product.js
@@ -1,7 +1,13 @@
-export default class product{
-    static id;
+let id=0
+function generarId() {
+    return id++
+  }
+  
+
+export class Product{
+
     constructor(title,description,price,thumbnail,code,stock){
-        this.id=this.id++
+        this.id=generarId()
         this.title=title
         this.description=description
         this.price=price
@@ -10,47 +16,16 @@ export default class product{
         this.stock=stock
     }
 
-    get id(){
-        return this.id
-    }
-    set id(newId){
-        this.id=newId
-    }
-
-    get title(){
-        return this.title
-    }
-    set title(newTitle){
-        this.title=newTitle
-    }
-    get description(){
-        return this.description
-    }
-    set description(newDescription){
-        this.description=newDescription
-    }
-    get price(){
-        return this.price
-    }
-    set price(newPrice){
-        this.price=newPrice
-    }
-    get thumbnail(){
-        return this.thumbnail
-    }
-    set thumbnail(newThumbnail){
-        this.thumbnail=newThumbnail
-    }
-    get code(){
-        return this.code
-    }
-    set code(newcode){
-        this.code=newcode
-    }
-    get stock(){
-        return this.stock
-    }
-    set stock(newStock){
-        this.stock=newStock
+    toPojo(){
+        const pojo = {
+            id: this.id,
+            title: this.title,
+            price: this.price,
+            description: this.description,
+            thumbnail: this.thumbnail,
+            stock: this.stock,
+            code: this.code,
+          };
+          return pojo;  
     }
 }
--- BackEnd/clase3/productManager/productManager.js
+++ BackEnd/clase3/productManager/productManager.js
@@ -1,145 +1,142 @@
 import fs from 'fs/promises'
-//import product from './product'
+import { Product } from './product.js'
 
 
-let id=1
-function generarId() {
-    return id++
-  }
-  
-  
-class productManager{
-    #products=[]
+
+
+class productManager {
+    #products = []
     #ruta
-    constructor(ruta){
-        this.#products=[]
-        this.#ruta=`C:/Users/jpola/Proyects/BackEnd/clase3/productManager/db/${ruta}.json`
-  }
+    constructor(ruta) {
+        this.#products = []
+        this.#ruta = `${ruta}.json`
+        fs.writeFile(this.#ruta, JSON.stringify(this.#products))
+    }
 
 
-//Validar Campos de los productos
-    validarCampos(title,description,price,thumbnail,code,stock)
-        {
-            if(title === null || title === undefined || title==='') {
-                console.log('Error: el titulo no puede estar vacio, ser null o undefined')
+    //Validar Campos de los productos
+    validarCampos(title, description, price, thumbnail, code, stock) {
+        if (title === null || title === undefined || title === '') {
+            console.log('Error: el titulo no puede estar vacio, ser null o undefined')
+            return false
+        } else {
+            if (description === null || description === undefined || description === '') {
+                console.log('Error: la descripcion no puede estar vacia, ser null o undefined')
                 return false
-            }else{
-                    if(description === null || description === undefined || description === '') {
-                        console.log('Error: la descripcion no puede estar vacia, ser null o undefined')
+            } else {
+                if (price === null || price === undefined || price === '') {
+                    console.log('Error: el precio no puede estar vacio, ser null o undefined')
+                    return false
+                } else {
+                    if (code === null || code === undefined || code === '') {
+                        console.log('Error: el codigo no puede estar vacio, ser null o undefined')
                         return false
-                    }else
-                      {
-                        if(price === null || price === undefined|| price === '' ) {
-                            console.log('Error: el precio no puede estar vacio, ser null o undefined')
+                    } else {
+                        if (thumbnail === null || thumbnail === undefined || thumbnail === '') {
+                            console.log('Error: la imagen no puede estar vacio, ser null o undefined')
                             return false
-                        }else
-                          {
-                            if(code === null || code === undefined || code === ''  ) {
-                                console.log('Error: el codigo no puede estar vacio, ser null o undefined')
+                        } else {
+                            if (stock === null || stock === undefined || stock === '') {
+                                console.log('Error: el stock no puede estar vacio, ser null o undefined')
                                 return false
-                            }else
-                                {
-                                    if(thumbnail === null || thumbnail === undefined || thumbnail==='') 
-                                        {
-                                            console.log('Error: la imagen no puede estar vacio, ser null o undefined')
-                                            return false
-                                        }else
-                                            {
-                                                if(stock === null || stock === undefined || stock === '') {
-                                                    console.log('Error: el stock no puede estar vacio, ser null o undefined')
-                                                    return false
-                                            }else{
-                                                return true
-                                            }
-                                        }
-                                }
+                            } else {
+                                return true
+                            }
                         }
                     }
                 }
-        } 
-
-
-//Si el codigo existe, devuelve el Producto, sino devuelve undefine
-    validarCodigo(codigo)
-        {
-            return (this.#products.find(function estaElCodigo(product) {
-                    return product.code === codigo;
-                }))
-        }
-    
-// Si el nuevo producto es valido y no esta repetido , lo agrega al arreglo
-    addProduct(newProduct)
-        {
-            if(this.validarCampos(newProduct.title,newProduct.description,newProduct.price,newProduct.thumbnail,newProduct.code,newProduct.stock)){
-                if(this.validarCodigo(newProduct.code) === undefined){
-                    newProduct.id=generarId()
-                    this.#products.push(newProduct)
-                    this.reset()
-                }else{
-                    console.log('Ya existe un producto con el mismo codigo')
-                }
             }
-            
-    
+        }
+    }
+
+
+    //Si el codigo existe, devuelve el Producto, sino devuelve undefine
+    validarCodigo(codigo) {
+        return (this.#products.find(function estaElCodigo(product) {
+            return product.code === codigo;
+        }))
+    }
+
+    // Si el nuevo producto es valido y no esta repetido , lo agrega al arreglo
+    addProduct(newProduct) {
+        if (this.validarCampos(newProduct.title, newProduct.description, newProduct.price, newProduct.thumbnail, newProduct.code, newProduct.stock)) {
+            if (this.validarCodigo(newProduct.code) === undefined) {
+                this.#products.push(newProduct)
+                this.reset()
+            } else {
+                console.log('Ya existe un producto con el mismo codigo')
+            }
         }
 
-    getProducts(){
+
+    }
+
+    getProducts() {
         return [...this.#products]
     }
-    getProductById(id){
-        let product=this.#products.find(function buscarProducto(product) {
-            return product.id === id});
-        if(product === undefined){
+    getProductById(id) {
+        let product = this.#products.find(function buscarProducto(product) {
+            return product.id === id
+        });
+        if (product === undefined) {
             return 'No se encontro el producto con el ID'
         }
-        else{
+        else {
             return product
         }
     }
 
-     reset(){
+    async reset() {
+        
+        await fs.unlink(this.#ruta)
+        console.log(this.#ruta)
+        await fs.writeFile("", JSON.stringify(this.#products))
     
-         fs.writeFile (this.#ruta,JSON.stringify(this.#products))
     }
+    async borrarlink() {
+        console.log(this.#ruta)
+      
+    }
+    async deleateProductById(id) {
 
-    deleateProductById(id){
         const newProduct = this.#products.filter(product => product.id !== id);
-        console.log(newProduct)
-        this.#products=newProduct
-        console.log(this.#products)
+        this.#products = newProduct
         this.reset()
     }
-  
-}
-   
 
-const p1={
-        title:'T1',
-        description:'D1',
-        price:5,
-        thumbnail:'2',
-        code:'5',
-        stock:5
+
 }
-const p2={
-    title:'T2',
-    description:'D2',
-    price:5,
-    thumbnail:'2',
-    code:'1',
-    stock:5
+
+
+const p1 = {
+    id: 1,
+    title: 'T1',
+    description: 'D1',
+    price: 5,
+    thumbnail: '2',
+    code: '5',
+    stock: 5
 }
-const p3={
-    title:'T3',
-    description:'D3',
-    price:5,
-    thumbnail:'3',
-    code:'3',
-    stock:5
+const p2 = {
+    id: 2,
+    title: 'T2',
+    description: 'D2',
+    price: 5,
+    thumbnail: '2',
+    code: '1',
+    stock: 5
 }
-const pm=new productManager("baseDeDatos")
+const p3 = {
+    id: 3,
+    title: 'T3',
+    description: 'D3',
+    price: 5,
+    thumbnail: '3',
+    code: '3',
+    stock: 5
+}
+const pm = new productManager("productsDB")
 pm.addProduct(p1)
 pm.addProduct(p2)
 pm.addProduct(p3)
-pm.deleateProductById(2)
-
+pm.deleateProductById(1)
